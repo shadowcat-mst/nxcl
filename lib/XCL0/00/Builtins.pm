04cbe71ffcfb0567050a664312fd11792fd8b96f
Matt S Trout <mst@shadowcat.co.uk>
2020-09-30 01:38:12 +0000

    re-arrange Builtins.pm

--- lib/XCL0/00/Builtins.pm
+++ lib/XCL0/00/Builtins.pm
@@ -15,6 +15,11 @@ our @EXPORT = qw(builtin_scope);
 
 my %raw = (
   _getscope => sub ($scope, $) { $scope },
+  _escape => sub ($scope, $lst) { car $lst },
+  _id => wrap sub ($scope, $lst) { car $lst },
+  _list => wrap sub ($scope, $lst) { $lst },
+  _type => wrap sub ($scope, $lst) { mkv String => chars => type(car $lst) },
+
   _wutcol => sub ($scope, $lst) {
     my ($if, $blocks) = uncons $lst;
     my ($then, $else) = uncons $blocks;
@@ -25,35 +30,9 @@ my %raw = (
       return eval_inscope $scope, car $else;
     }
   },
+
   _eval_inscope => wrap \&eval_inscope,
-  _rmkraw => wrap sub ($scope, $lst) {
-    my ($typep, $reprp, $v) = flatten $lst;
-    mkv(raw($typep), raw($reprp), raw($v));
-  },
-  _rmkref => wrap sub ($scope, $lst) {
-    my ($typep, $reprp, @v) = flatten($lst);
-    mkv(raw($typep), raw($reprp), @v);
-  },
-  _rmknil => wrap sub ($scope, $lst) {
-    mkv(raw(car($lst)), 'nil');
-  },
-  _rtrue => sub ($, $) { mkv(Bool => bool => 1) },
-  _rfalse => sub ($, $) { mkv(Bool => bool => 0) },
-  _escape => sub ($scope, $lst) { car $lst },
-  _id => wrap sub ($scope, $lst) { car $lst },
-  _list => wrap sub ($scope, $lst) { $lst },
-  _type => wrap sub ($scope, $lst) { mkv String => chars => type(car $lst) },
-  _rtype => wrap sub ($scope, $lst) { mkv String => chars => rtype(car $lst) },
-  (map {
-    my $code = XCL0::00::Runtime->can("r${_}p");
-    ("_r${_}?" => wrap sub ($scope, $lst) {
-      mkv Bool => bool => 0+!!$code->(car $lst) 
-    })
-  } qw(cons nil chars bool native val var)),
-  (map {
-    my $code = XCL0::00::Runtime->can($_);
-    ('_'.$_ => wrap sub ($scope, $lst) { $code->(car $lst) })
-  } qw(valp refp val deref car cdr)),
+
   _wrap => wrap sub ($scope, $lst) {
     mkv Native => native => wrap(raw(car $lst))
   },
@@ -81,6 +60,35 @@ my %raw = (
     my ($l, $r) = (car($lst), car($lst, 1));
     die unless type($l) eq 'String' and type($r) eq 'String';
     mkv(String => chars => raw($l).raw($r))
+  },
+
+  _rtype => wrap sub ($scope, $lst) { mkv String => chars => rtype(car $lst) },
+
+  _rmkraw => wrap sub ($scope, $lst) {
+    my ($typep, $reprp, $v) = flatten $lst;
+    mkv(raw($typep), raw($reprp), raw($v));
+  },
+  _rmkref => wrap sub ($scope, $lst) {
+    my ($typep, $reprp, @v) = flatten($lst);
+    mkv(raw($typep), raw($reprp), @v);
+  },
+  _rmknil => wrap sub ($scope, $lst) {
+    mkv(raw(car($lst)), 'nil');
+  },
+  _rtrue => sub ($, $) { mkv(Bool => bool => 1) },
+  _rfalse => sub ($, $) { mkv(Bool => bool => 0) },
+  (map {
+    my $code = XCL0::00::Runtime->can("r${_}p");
+    ("_r${_}?" => wrap sub ($scope, $lst) {
+      mkv Bool => bool => 0+!!$code->(car $lst) 
+    })
+  } qw(cons nil chars bool native val var)),
+  (map {
+    my $code = XCL0::00::Runtime->can($_);
+    ('_'.$_ => wrap sub ($scope, $lst) { $code->(car $lst) })
+  } qw(valp refp val deref car cdr)),
+  _wrap => wrap sub ($scope, $lst) {
+    mkv Native => native => wrap(raw(car $lst))
   },
 );
 
