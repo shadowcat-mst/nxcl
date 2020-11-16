942d590e711ce61438e060cfef96386efad8afab
Matt S Trout <mst@shadowcat.co.uk>
2020-11-16 23:43:38 +0000

    fatalize undef warnings for Exporter.pm

--- lib/XCL0/Exporter.pm
+++ lib/XCL0/Exporter.pm
@@ -5,7 +5,7 @@ use Import::Into;
 
 sub import {
   Mojo::Base->import::into(1, -strict, -signatures);
-  warnings->import::into(1, 'uninitialized');
+  warnings->import::into(1, FATAL => 'uninitialized');
   Exporter->import::into(1, qw(import));
 }
 
