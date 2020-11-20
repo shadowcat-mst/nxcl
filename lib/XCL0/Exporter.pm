74ee1e72b8ca875042e3de360aca4b326e6c610d
Matt S Trout <mst@shadowcat.co.uk>
2020-11-20 01:35:24 +0000

    kill Mojo::Base usage for the moment

--- lib/XCL0/Exporter.pm
+++ lib/XCL0/Exporter.pm
@@ -1,10 +1,14 @@
 package XCL0::Exporter;
 
-use Mojo::Base -strict;
+use strict;
+use warnings;
 use Import::Into;
 
 sub import {
-  Mojo::Base->import::into(1, -strict, -signatures);
+  strict->import::into(1);
+  warnings->import::into(1);
+  feature->import::into(1, ':5.16');
+  experimental->import::into(1, 'signatures');
   warnings->import::into(1, FATAL => 'uninitialized');
   Exporter->import::into(1, qw(import));
 }
