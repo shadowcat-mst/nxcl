2f1de14788b9efec7f665e031433b7b5fe30c47f
Matt S Trout <mst@shadowcat.co.uk>
2021-02-03 02:30:42 +0000

    clean up Environment

--- lib/NXCL/01/Environment.pm
+++ lib/NXCL/01/Environment.pm
@@ -8,11 +8,15 @@ use NXCL::01::TypeFunctions
     Name Int String Combine List BlockProto Call
   );
 
-lazy scope => sub { require NXCL::01::BaseScope; NXCL::01::BaseScope::scope };
-lazy reader => sub { require NXCL::01::Reader; NXCL::01::Reader->new };
+lazy scope => sub {
+   require NXCL::01::BaseScope; NXCL::01::BaseScope::scope()
+};
+lazy reader => sub {
+  require NXCL::01::Reader; NXCL::01::Reader->new
+};
 lazy makers => sub {
   +{
-    map +($_ => __PACKAGE__->("make_${_}")), @EXPAND_TYPES
+    map +($_ => __PACKAGE__->can("make_${_}")), @EXPAND_TYPES
   }
 };
 lazy expander => sub ($self) {
@@ -25,9 +29,9 @@ sub _run ($self, $value, $kstack) {
 }
 
 sub eval_string ($self, $string) {
-  my $parse = $self->reader->parse($string);
+  my $parse = $self->reader->from_string($string);
   my $script = $self->expander->expand($parse);
-  $self->_run([ EVAL => $self->scope, $script ], make_List());
+  $self->_run([ EVAL => $self->scope, $script ], make_List([ 'HOST' ]));
 }
 
 sub resume ($self, $value, $kstack) {
