cb7567de13cab69caf4b618d71f7006f4072c122
Matt S Trout <mst@shadowcat.co.uk>
2021-04-24 04:50:59 +0000

    trace ids

--- lib/NXCL/01/RunTrace.pm
+++ lib/NXCL/01/RunTrace.pm
@@ -12,13 +12,22 @@ sub import { $Max = $_[1] if defined $_[1] }
 
 sub NXCL::01::Runtime::DEBUG { 1 }
 
+use NXCL::01::ScopeT;
+use Hash::Util qw(fieldhash);
+fieldhash my %scopes;
+BEGIN {
+  my $idx = 'A000';
+  for ($NXCL::01::TypeRegistry::TypeInfo{Scope}->exports->{make_Scope}) {
+    $_ = do { my $v = $_; sub { my $s = &$v; $scopes{$s} = ++$idx; $s } }
+  }
+}
 use NXCL::01::Utils qw(uncons mset raw);
 use NXCL::01::TypeFunctions qw(Scope_Inst);
 use NXCL::01::JSON;
 
 sub jsonify ($v) {
   if (mset($v) == Scope_Inst) {
-    return [ Scope => sprintf("%x", 0+raw($v)) ];
+    return [ Scope => 'SCOPE_'.$scopes{$v} ];
   }
   return nxcl2json($v);
 }
