1c998a5e3d347fc5e910876c3031ab7310da1e6d
Matt S Trout <mst@shadowcat.co.uk>
2021-04-25 09:17:58 +0000

    move method op bodies to MethodUtils

--- lib/NXCL/01/BaseScope.pm
+++ lib/NXCL/01/BaseScope.pm
@@ -1,29 +1,21 @@
 package NXCL::01::BaseScope;
 
 use NXCL::Package;
-use NXCL::01::Utils qw(panic flatten mset raw uncons);
 use NXCL::01::MethodUtils;
 use vars qw(@BASE_TYPES);
-use NXCL::01::TypeFunctions
+use NXCL::01::TypeFunctions (
   (@BASE_TYPES = qw(
     Apv Bool Combine Curry Int List Name Native
     OpDict Scope String Val Compound
   )),
-  qw(
-    Name_Inst String_Inst Int_Inst
-    make_Apv make_Val make_OpDict make_List make_Native make_Curry
-    make_String cons_List make_Scope
-  );
+  qw(make_Val make_Scope make_Native make_OpDict),
+);
 
-our %N =
-  map +($_ => make_Native(__PACKAGE__->can($_))),
-    qw(dot_lookup dot_curryable dot_curried fdot dot);
-
-$_ = make_Apv($_) for $N{dot_curried};
+my $val_dot = make_Val make_Native \&dot;
 
 our $Store = make_OpDict +{
-  dot => make_Val($N{dot}),
-  '.' => make_Val($N{dot}),
+  dot => make_Val($ndot),
+  '.' => make_Val($ndot),
   map +($_ => make_Val(__PACKAGE__->can($_)->())),
     @BASE_TYPES
 };
@@ -31,81 +23,5 @@ our $Store = make_OpDict +{
 our $Scope = make_Scope $Store;
 
 sub scope () { $Scope }
-
-sub dot_lookup ($scope, $, $args, $kstack) {
-  my ($lookup, $obj) = flatten $args;
-  return (
-    [ CMB9 => $scope => $obj => make_List($lookup) ],
-    $kstack
-  );
-}
-
-sub dot_curryable ($scope, $, $args, $kstack) {
-  return (
-    [ JUST => make_Curry($N{dot_curried}, $args) ],
-    $kstack
-  );
-}
-
-sub dot_curried ($scope, $, $argsp, $kstack) {
-  my ($method, $args) = uncons $argsp;
-  my ($obj) = uncons $args;
-  call_method($scope, $obj, $method, $args, $kstack);
-}
-
-sub fdot ($scope, $, $args, $kstack) {
-  my ($callp, $obj) = flatten $args;
-  my $ctype = mset($callp);
-  if ($ctype == Name_Inst) {
-    my $method = make_String raw($callp);
-
-    if ($obj) {
-      return lookup_method($scope, $obj, $method, $kstack);
-    }
-
-    return (
-      [ JUST => make_Curry($N{dot_curryable}, make_List $method) ],
-      $kstack
-    );
-  }
-
-  panic unless $ctype == String_Inst or $ctype == Int_Inst;
-
-  if ($obj) {
-    return (
-      [ CMB9 => $scope => $obj => make_List($callp) ],
-      $kstack
-    );
-  }
-
-  return (
-    [ JUST => make_Curry($N{dot_lookup}, make_List $callp) ],
-    $kstack
-  );
-}
-
-sub dot ($scope, $cmb, $args, $kstack) {
-  my @args = flatten $args;
-  panic unless @args == 1 or @args == 2;
-  my $mset = mset($args[-1]);
-  if ($mset == Name_Inst or $mset == Int_Inst or $mset == String_Inst) {
-
-    unless (@args == 2) {
-      return fdot($scope, undef, $args, $kstack);
-    }
-
-    return (
-      [ EVAL => $scope => make_List($args[0]) ],
-      [ CONS => $args[-1] ],
-      [ CMB9 => $scope => $N{fdot} ],
-      $kstack
-    );
-  }
-  return (
-    [ EVAL => $scope, $args ],
-    [ CMB9 => $scope => $N{fdot} ],
-    $kstack
-  );
-}
 
 1;
--- lib/NXCL/01/MethodUtils.pm
+++ lib/NXCL/01/MethodUtils.pm
@@ -1,14 +1,22 @@
 package NXCL::01::MethodUtils;
 
 use NXCL::Exporter;
-use NXCL::01::Utils qw(panic mset raw);
+use NXCL::01::Utils qw(panic mset raw flatten uncons);
 use NXCL::01::TypeFunctions qw(
-  OpDict_Inst Native_Inst
-  make_List make_String make_Curry make_Native
+  OpDict_Inst Native_Inst Name_Inst String_Inst Int_Inst
+  make_List make_String make_Curry make_Native make_Apv
 );
 use NXCL::01::TypeRegistry;
 
-our @EXPORT = qw(call_method lookup_method);
+our @EXPORT = qw(call_method lookup_method $ndot);
+
+our $ndot = make_Native \&dot;
+
+our %N =
+  map +($_ => make_Native(__PACKAGE__->can($_))),
+    qw(dot_lookup dot_curryable dot_curried fdot);
+
+$_ = make_Apv($_) for $N{dot_curried};
 
 sub call_method ($scope, $self, $methodp, $args, $kstack) {
   my ($method_name, $method_String) = (
@@ -59,6 +67,82 @@ sub lookup_method ($scope, $self, $methodp, $kstack) {
     [ CMB9 => $scope => make_Native(sub ($scope, $cmb, $args, $kstack) {
         make_Curry((uncons($args))[0], make_List($self))
     }) ],
+    $kstack
+  );
+}
+
+sub dot_lookup ($scope, $, $args, $kstack) {
+  my ($lookup, $obj) = flatten $args;
+  return (
+    [ CMB9 => $scope => $obj => make_List($lookup) ],
+    $kstack
+  );
+}
+
+sub dot_curryable ($scope, $, $args, $kstack) {
+  return (
+    [ JUST => make_Curry($N{dot_curried}, $args) ],
+    $kstack
+  );
+}
+
+sub dot_curried ($scope, $, $argsp, $kstack) {
+  my ($method, $args) = uncons $argsp;
+  my ($obj) = uncons $args;
+  call_method($scope, $obj, $method, $args, $kstack);
+}
+
+sub fdot ($scope, $, $args, $kstack) {
+  my ($callp, $obj) = flatten $args;
+  my $ctype = mset($callp);
+  if ($ctype == Name_Inst) {
+    my $method = make_String raw($callp);
+
+    if ($obj) {
+      return lookup_method($scope, $obj, $method, $kstack);
+    }
+
+    return (
+      [ JUST => make_Curry($N{dot_curryable}, make_List $method) ],
+      $kstack
+    );
+  }
+
+  panic unless $ctype == String_Inst or $ctype == Int_Inst;
+
+  if ($obj) {
+    return (
+      [ CMB9 => $scope => $obj => make_List($callp) ],
+      $kstack
+    );
+  }
+
+  return (
+    [ JUST => make_Curry($N{dot_lookup}, make_List $callp) ],
+    $kstack
+  );
+}
+
+sub dot ($scope, $cmb, $args, $kstack) {
+  my @args = flatten $args;
+  panic unless @args == 1 or @args == 2;
+  my $mset = mset($args[-1]);
+  if ($mset == Name_Inst or $mset == Int_Inst or $mset == String_Inst) {
+
+    unless (@args == 2) {
+      return fdot($scope, undef, $args, $kstack);
+    }
+
+    return (
+      [ EVAL => $scope => make_List($args[0]) ],
+      [ CONS => $args[-1] ],
+      [ CMB9 => $scope => $N{fdot} ],
+      $kstack
+    );
+  }
+  return (
+    [ EVAL => $scope, $args ],
+    [ CMB9 => $scope => $N{fdot} ],
     $kstack
   );
 }
