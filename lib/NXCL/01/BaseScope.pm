867b1abc59e47f10bd597798f94313896cd26807
Matt S Trout <mst@shadowcat.co.uk>
2021-04-28 02:38:17 +0000

    update BaseScope to provide curried plus and minus

--- lib/NXCL/01/BaseScope.pm
+++ lib/NXCL/01/BaseScope.pm
@@ -1,6 +1,7 @@
 package NXCL::01::BaseScope;
 
 use NXCL::Package;
+use NXCL::01::Utils qw(uncons);
 use NXCL::01::MethodUtils;
 use vars qw(@BASE_TYPES);
 use NXCL::01::TypeFunctions (
@@ -11,9 +12,21 @@ use NXCL::01::TypeFunctions (
   qw(make_Val make_Scope make_Native make_OpDict),
 );
 
+my %opmeth = map {
+  my ($opname, $opmeth) = @$_;
+  ($opname => make_Val make_Native sub ($scope, $, $args, $kstack) {
+    my ($obj) = uncons $args;
+    call_method($scope, $obj, $opmeth, $args, $kstack);
+  })
+} (
+  [ '+', 'plus' ],
+  [ '-', 'minus' ],
+);
+
 our $Store = make_OpDict +{
   dot => make_Val($DOT),
   '.' => make_Val($DOT),
+  %opmeth,
   map +($_ => make_Val(__PACKAGE__->can($_)->())),
     @BASE_TYPES
 };
