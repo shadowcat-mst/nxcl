a00a5a2a2cde674b6d26b9065be51e1f622cb3ed
Matt S Trout <mst@shadowcat.co.uk>
2021-05-12 23:12:37 +0000

    add to_xcl_string to NativeT

--- lib/NXCL/NativeT.pm
+++ lib/NXCL/NativeT.pm
@@ -2,11 +2,18 @@ package NXCL::NativeT;
 
 use NXCL::Utils qw(raw);
 use NXCL::ReprTypes qw(NativeR);
+use NXCL::TypeFunctions qw(make_String);
+use Sub::Util qw(subname);
 use NXCL::TypePackage;
 
 export make => \&make;
 
 sub make ($sub) { _make NativeR ,=> $sub }
+
+method to_xcl_string => sub ($scope, $cmb, $self, $) {
+  # should indirect via Combine maybe?
+  return ([ JUST => make_String("Native('".subname(\&{raw($self)})."')") ]);
+};
 
 method combine => sub ($scope, $cmb, $self, $args) {
   raw($self)->($scope, $self, $args);
