ca4be8ecbd96ffe956a8447cbf013527cb464275
Matt S Trout <mst@shadowcat.co.uk>
2021-05-12 22:21:42 +0000

    fixup string ops (concat, gt, eq)

--- lib/NXCL/StringT.pm
+++ lib/NXCL/StringT.pm
@@ -1,6 +1,6 @@
 package NXCL::StringT;
 
-use NXCL::Utils qw(panic flatten raw);
+use NXCL::Utils qw(panic flatten raw mset);
 use NXCL::ReprTypes qw(CharsR);
 use NXCL::TypeFunctions qw(make_Bool);
 use NXCL::TypePackage;
@@ -17,29 +17,29 @@ method to_xcl_string => sub ($scope, $cmb, $self, $) {
 wrap method eq => sub ($scope, $cmb, $self, $args) {
   my ($r, @too_many) = flatten $args;
   panic 'Too many args' if @too_many;
-  my $type = type($self);
-  panic 'Must be strings' unless type($r) == $type;
+  my $mset = mset($self);
+  panic 'Must be strings' unless mset($r) == $mset;
   return (
-    make_Bool(raw($self) eq raw($r)),
+    [ JUST => make_Bool(raw($self) eq raw($r)) ],
   );
 };
 
 wrap method gt => sub ($scope, $cmb, $self, $args) {
   my ($r, @too_many) = flatten $args;
   panic 'Too many args' if @too_many;
-  my $type = type($self);
-  panic 'Must be strings' unless type($r) == $type;
+  my $mset = mset($self);
+  panic 'Must be strings' unless mset($r) == $mset;
   return (
-    make_Bool(raw($self) gt raw($r)),
+    [ JUST => make_Bool(raw($self) gt raw($r)) ],
   );
 };
 
 wrap method concat => sub ($scope, $cmb, $self, $args) {
   my @string = flatten $args;
-  my $type = type($self);
-  panic 'Must be strings' for grep type($_) != $type, @string;
+  my $mset = mset($self);
+  panic 'Must be strings' for grep mset($_) != $mset, @string;
   return (
-    _make(join '', map raw($_). $self, @string),
+    [ JUST => make(join '', map raw($_), $self, @string) ]
   );
 };
 
