19487caac1870a8a81b7ba3559be38faa9845bca
Matt S Trout <mst@shadowcat.co.uk>
2021-05-15 05:46:50 +0000

    ApMethT and CurryT to_xcl_string

--- lib/NXCL/ApMethT.pm
+++ lib/NXCL/ApMethT.pm
@@ -2,10 +2,20 @@ package NXCL::ApMethT;
 
 use NXCL::Utils qw(uncons raw);
 use NXCL::ReprTypes qw(ValR);
-use NXCL::TypeFunctions qw(cons_List);
+use NXCL::TypeFunctions qw(cons_List make_List empty_List make_String);
 use NXCL::TypePackage;
 
 export make => sub ($opv) { _make ValR ,=> $opv };
+
+method to_xcl_string => sub ($scope, $, $self, $) {
+  state $fmt = make_String('ApMeth(%s)');
+  return (
+    [ CALL => $scope => 'to_xcl_string' => make_List(raw($self)) ],
+    [ SNOC => empty_List ],
+    [ CONS => $fmt ],
+    [ CALL => $scope => 'sprintf' ],
+  );
+};
 
 method combine => sub ($scope, $cmb, $self, $args) {
   my ($inv, $method_args) = uncons($args);
--- lib/NXCL/CurryT.pm
+++ lib/NXCL/CurryT.pm
@@ -2,13 +2,24 @@ package NXCL::CurryT;
 
 use NXCL::Utils qw(uncons flatten rnilp);
 use NXCL::ReprTypes qw(ConsR);
-use NXCL::TypeFunctions qw(cons_List make_List);
+use NXCL::TypeFunctions qw(cons_List make_List make_String empty_List);
 use NXCL::TypePackage;
 
 export make => sub ($cmb, @i_args) { cons($cmb, make_List @i_args) };
 export cons => \&cons;
 
 sub cons ($cmb, $i_args) { _make ConsR ,=> $cmb, $i_args }
+
+method to_xcl_string => sub ($scope, $, $self, $) {
+  state $fmt = make_String('Curry%s');
+  return (
+    [ CALL => $scope => 'to_xcl_string'
+      => make_List(make_List(flatten($self))) ],
+    [ SNOC => empty_List ],
+    [ CONS => $fmt ],
+    [ CALL => $scope => 'sprintf' ],
+  );
+};
 
 # called args versus implicit args - c_args versus i_args
 
