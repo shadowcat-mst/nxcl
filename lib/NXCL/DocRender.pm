de1a628521522207d71a539bca948dea7983b4fc
Matt S Trout <mst@shadowcat.co.uk>
2021-06-16 01:56:14 +0000

    tighten up scoping for DocRender

--- lib/NXCL/DocRender.pm
+++ lib/NXCL/DocRender.pm
@@ -1,16 +1,21 @@
 package NXCL::DocRender;
 
 use NXCL::Exporter;
-use NXCL::YDump;
 
 our @EXPORT = qw(render);
 
 sub render ($eval, $code) {
+  package NXCL::DocRender::In;
   return eval($eval) // die "Eval failed for ${code} using ${eval}: $@";
 }
 
+package NXCL::DocRender::In;
+
+use NXCL::YDump;
+
 sub Read ($data) {
-  nxcl_require('NXCL::Reader')->new->parse(script => $data);
+  state $reader = do { require NXCL::Reader; NXCL::Reader->new };
+  $reader->parse(script => $data);
 }
 
 1;
