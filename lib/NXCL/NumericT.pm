e7ce5c081208e9c3748777b9d13eb566a89433ca
Matt S Trout <mst@shadowcat.co.uk>
2021-06-18 00:41:23 +0000

    NumericT

---
+++ lib/NXCL/NumericT.pm
@@ -0,0 +1,17 @@
+package NXCL::NumericT;
+
+use NXCL::Utils qw(panic flatten raw mset);
+use NXCL::ReprTypes qw(CharsR);
+use NXCL::TypeFunctions qw(make_Bool);
+use NXCL::TypePackage;
+
+export make => \&make;
+
+sub make ($string) { _make CharsR, => $string }
+
+method to_xcl_string => sub ($scope, $cmb, $self, $) {
+  # this is wrong
+  JUST make("N'".raw($self)."'");
+};
+
+1;
