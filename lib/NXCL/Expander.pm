3cc5cfd3fc91e8cccc8654a85cd30c2cfc238fed
Matt S Trout <mst@shadowcat.co.uk>
2021-06-22 17:24:03 +0000

    rebuild expander code

--- lib/NXCL/Expander.pm
+++ lib/NXCL/Expander.pm
@@ -2,67 +2,58 @@ package NXCL::Expander;
 
 use NXCL::Class;
 
-ro 'makers';
+lazy maker => nxcl_require_and_call('NXCL::TypeMaker', can => 'make_value');
+
+sub make ($self, @v) { $self->maker->(@v) }
 
 sub expand ($self, $v) {
-  my ($tok_type, $payload) = @$v; # later also $meta
-  $self->${\"expand_${tok_type}"}($payload);
+  my ($tok_type, @payload) = @$v; # later also $meta
+  $self->${\"expand_${tok_type}"}(@payload);
 }
 
-sub make ($self, $type, @v) {
-  ($self->makers->{$type}||die"No maker for ${type}")->(@v);
+sub expand_ws { () }
+sub expand_comment { () }
+sub expand_comma { () }
+sub expand_semicolon { () }
+
+sub expand_word ($self, $name) { $self->make(Name => $name) }
+
+sub expand_symbol ($self, $name) { $self->make(Name => $name) }
+
+sub expand_numeric ($self, $v) { $self->make(Numeric => $v) }
+
+sub expand_string ($self, $v) { $self->make(String => $v) }
+
+sub expand_compound ($self, @v) {
+  return $self->expand($v[0]) unless @v > 1;
+  $self->make(Compound => map $self->expand($_), @v);
 }
 
-sub expand_word ($self, $name) {
-  $self->make(Name => $name);
+sub expand_list ($self, @v) {
+  $self->make(List => map $self->expand($_), @v);
 }
 
-sub expand_symbol ($self, $name) {
-  $self->make(Name => $name);
+sub expand_block ($self, @v) {
+  $self->make(BlockProto => map $self->expand($_), @v);
 }
 
-sub expand_numeric ($self, $v) {
-  $self->make(Numeric => $v);
-}
-
-sub expand_string ($self, $v) {
-  $self->make(String => $v);
-}
-
-sub expand_compound ($self, $v) {
-  return $self->expand($v->[0]) unless @$v > 1;
-  $self->make(Compound => map $self->expand($_), @$v);
-}
-
-sub expand_list ($self, $v) {
-  $self->make(List => map $self->expand($_), @$v);
-}
-
-sub expand_block ($self, $v) {
-  $self->make(BlockProto => map $self->expand($_), @$v);
-}
-
-sub expand_call ($self, $v) {
+sub expand_call ($self, @v) {
   # handle the [x] case
-  if (@$v == 1 and @{$v->[0][1]} == 1) { # guaranteed to be an expr node
+  if (@v == 1 and @{$v[0][1]} == 1) { # guaranteed to be an expr node
     return $self->make(Call => [
-      $self->make(Combine => $self->expand($v->[0][1][0])),
+      $self->make(Combine => $self->expand($v[0][1][0])),
     ]);
   }
-  $self->make(Call => map $self->expand($_), @$v);
+  $self->make(Call => map $self->expand($_), @v);
 }
 
-sub expand_script ($self, $v) {
-  $self->make(Call => map $self->expand($_), @$v);
+sub expand_script ($self, @v) {
+  $self->make(Call => map $self->expand($_), @v);
 }
 
-sub expand_expr ($self, $v) {
-  return $self->expand($v->[0]) unless @$v > 1;
-  my ($firstp, @restp) = @$v;
-  $self->make(Combine =>
-    $self->expand($firstp),
-    map $self->expand($_), @restp
-  );
+sub expand_expr ($self, @v) {
+  return $self->expand($v[0]) unless @v > 1;
+  $self->make(Combine => map $self->expand($_), @v);
 }
 
 1;
