62169400a9b20c8b538b76f351980d9071a479bf
Matt S Trout <mst@shadowcat.co.uk>
2021-06-24 02:08:14 +0000

    cleanup boolean handling for ydump

--- lib/NXCL/YDump.pm
+++ lib/NXCL/YDump.pm
@@ -1,12 +1,19 @@
 package NXCL::YDump;
 
 use CPAN::Meta::YAML qw(Dump);
+use Safe::Isa;
 use NXCL::Exporter;
 
 our @EXPORT = qw(ydump);
 
 sub _cook ($type, @parts) {
-  my @cooked_parts = map { ref($_) ? __SUB__->(@$_) : $_ } @parts;
+  my @cooked_parts = map {
+    ref($_) eq 'ARRAY'
+      ? __SUB__->(@$_)
+      : $_->isa('JSON::PP::Boolean')
+        ? ($_ ? 'true' : 'false')
+        :  $_
+  } @parts;
   return { $type => (@cooked_parts > 1 ? \@cooked_parts : $cooked_parts[0]) };
 }
 
