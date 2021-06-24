538ba08dd34c48316aae8aa219d0379287a74a24
Matt S Trout <mst@shadowcat.co.uk>
2021-06-24 01:08:10 +0000

    avoid double imports for typemaker

--- lib/NXCL/TypeMaker.pm
+++ lib/NXCL/TypeMaker.pm
@@ -6,9 +6,12 @@ use NXCL::Exporter;
 our @EXPORT = qw(make_value);
 
 sub make_value ($type, @v) {
-  my $maker = "make_${type}";
-  NXCL::TypeFunctions->import($maker);
-  __PACKAGE__->can($maker)->(@v);
+  my $maker_name = "make_${type}";
+  my $maker = __PACKAGE__->can($maker_name) || do {
+    NXCL::TypeFunctions->import($maker_name);
+    __PACKAGE__->can($maker_name);
+  };
+  $maker->(@v);
 }
 
 1;
