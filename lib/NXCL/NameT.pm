3626d81c70b9c418b56624832439c92786a286bc
Matt S Trout <mst@shadowcat.co.uk>
2021-08-03 01:47:35 +0000

    move NameT evaluate logic into ScopeT combine

--- lib/NXCL/NameT.pm
+++ lib/NXCL/NameT.pm
@@ -3,9 +3,9 @@ package NXCL::NameT;
 use NXCL::Utils qw(mset object_is raw panic);
 use NXCL::ReprTypes qw(CharsR);
 use NXCL::TypeFunctions qw(
-  OpDict_Inst Val_Inst Var_Inst
-  make_String make_List empty_List
+  make_String make_List
 );
+use NXCL::MethodUtils qw(call_method);
 use NXCL::TypePackage;
 
 export make => \&make;
@@ -13,18 +13,9 @@ export make => \&make;
 sub make ($name) { _make CharsR ,=> $name }
 
 method evaluate => sub ($scope, $cmb, $self, $args) {
-  my $store = raw $scope;
-  if (object_is $store, OpDict_Inst) {
-    my $cell = raw($store)->{raw($self)};
-    panic "No value for ${\raw($self)} in current scope" unless $cell;
-    if (mset($cell) == Val_Inst or mset($cell) == Var_Inst) {
-      return JUST raw($cell);
-    }
-    return CMB9 $cell => empty_List;
-  }
-  return (
-    CMB9($store => make_List(make_String(raw($self)))),
-    CMB6(empty_List),
+  return call_method(
+    $scope, $scope,
+    combine => make_List($scope, make_String raw($self))
   );
 };
 
--- lib/NXCL/ScopeT.pm
+++ lib/NXCL/ScopeT.pm
@@ -1,8 +1,31 @@
 package NXCL::ScopeT;
 
 use NXCL::ReprTypes qw(VarR);
+use NXCL::Utils qw(mset object_is raw panic uncons);
+use NXCL::TypeFunctions qw(
+  OpDict_Inst Val_Inst Var_Inst
+  make_String make_List empty_List
+);
 use NXCL::TypePackage;
 
 export make => sub ($store) { _make VarR ,=> $store };
+
+method combine => sub ($scope, $cmb, $self, $args) {
+  my ($namep) = uncons($args);
+  my $name = raw($namep);
+  my $store = raw $self;
+  if (object_is $store, OpDict_Inst) {
+    my $cell = raw($store)->{$name};
+    panic "No value for ${name} in current scope" unless $cell;
+    if (mset($cell) == Val_Inst or mset($cell) == Var_Inst) {
+      return JUST raw($cell);
+    }
+    return CMB9 $cell => empty_List;
+  }
+  return (
+    CMB9($store => make_List make_String $name),
+    CMB6(empty_List),
+  );
+};
 
 1;
