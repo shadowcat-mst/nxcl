d267a86e784a302465fa8df246a9102284a7680d
Matt S Trout <mst@shadowcat.co.uk>
2021-11-03 02:41:54 +0000

    switch RV over to direct method calls

--- lib/NXCL/RV.pm
+++ lib/NXCL/RV.pm
@@ -1,26 +1,19 @@
 package NXCL::RV;
 
-use NXCL::WV;
 use NXCL::Class;
+use NXCL::ValueBuilders;
 use Autoload::AUTOCAN;
 
 ro 'raw_value';
-ro 'kstack';
-ro 'scope';
 ro 'env';
-
-lazy value => sub ($self) {
-  NXCL::WV->new(
-    _raw_xcl_value => $self->raw_value,
-    _xcl_scope => $self->scope,
-    _xcl_environment => $self->env,
-  );
-};
 
 sub AUTOCAN {
   my (undef, $method) = @_;
   return undef unless $method =~ s/^value_//;
-  return sub ($self) { $self->value->$method };
+  return sub ($self) {
+    my $mcall = Cmb( Call($method, $self->raw_value) );
+    $self->env->eval($mcall)
+  };
 }
 
 1;
