8b63f6d94dcc68259c7d8db96e5f56405eb72872
Matt S Trout <mst@shadowcat.co.uk>
2021-11-07 21:33:16 +0000

    fix IntroScope to actually use intro_as (facepalm)

--- lib/NXCL/IntroScopeT.pm
+++ lib/NXCL/IntroScopeT.pm
@@ -1,8 +1,11 @@
 package NXCL::IntroScopeT;
 
 use NXCL::ReprTypes qw(DictR);
-use NXCL::Utils qw(panic flatten raw object_is);
-use NXCL::TypeFunctions qw(cons_List make_OpDict OpDict_Inst);
+use NXCL::Utils qw(panic flatten uncons raw object_is);
+use NXCL::TypeFunctions qw(
+  cons_List empty_List
+  make_OpDict OpDict_Inst
+);
 use NXCL::TypePackage;
 
 sub make ($scope, $intro_as) {
@@ -21,13 +24,27 @@ method get_value_for_name => sub ($scope, $cmb, $self, $args) {
 };
 
 method set_value_for_name => sub ($scope, $cmb, $self, $args) {
-  my ($namep, $value) = flatten($args);
+  my ($namep, $vlist) = uncons($args);
+  my $intro_as = raw($self)->{intro_as};
+  return (
+    CALL(new => cons_List($intro_as, $vlist)),
+    SNOC(empty_List),
+    CONS($namep),
+    CONS($self),
+    CALL('set_cell_for_name'),
+    DROP(),
+    JUST((uncons($vlist))[0])
+  );
+};
+
+method set_cell_for_name => sub ($scope, $cmb, $self, $args) {
+  my ($namep, $cell) = flatten($args);
   my $store = raw(my $intscope = raw($self)->{scope});
   panic "NYI" unless object_is($store, OpDict_Inst);
   # this probably *could* mutate the hashref directly but meh
-  my $new_store = make_OpDict({ %{raw($store)}, raw($namep) => $value });
+  my $new_store = make_OpDict({ %{raw($store)}, raw($namep) => $cell });
   raw($intscope) = $new_store;
-  return JUST $value;
+  return JUST $cell;
 };
 
 1;
