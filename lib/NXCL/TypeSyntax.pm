21e0029f411b7770e1ab3d5bc061c19900ae39ad
Matt S Trout <mst@shadowcat.co.uk>
2021-12-21 01:53:51 +0000

    downgrade/optimise from Keyword::Declare to Keyword::Simple

--- lib/NXCL/TypeSyntax.pm
+++ lib/NXCL/TypeSyntax.pm
@@ -6,6 +6,45 @@ use NXCL::OpUtils;
 use Keyword::Declare;
 use curry;
 
+# use Keyword::Declare;
+# ...
+# keyword export (Ident $name, List $args, Block $body) {
+#   return qq{
+#     sub $name $args $body
+#     \$Type_Info->add_export("$name", \\\&${name});
+#   };
+# }
+# keyword static (Ident $name, Block $body) {
+#   return qq{
+#     \$Type_Info->add_static_apv($name => sub (\$self, \$args) $body);
+#   };
+# }
+# keyword staticx (Ident $name, Block $body) {
+#   return qq{
+#     \$Type_Info->add_static_opv($name => sub (\$self, \$args) $body);
+#   };
+# }
+# keyword staticn (Ident $name, Block $body) {
+#   return qq{
+#     \$Type_Info->add_static_opv($name => sub (\$self, \$) $body);
+#   };
+# }
+# keyword method (Ident $name, Block $body) {
+#   return qq{
+#     \$Type_Info->add_method_apv($name => sub (\$self, \$args) $body);
+#   };
+# }
+# keyword methodx (Ident $name, Block $body) {
+#   return qq{
+#     \$Type_Info->add_method_opv($name => sub (\$self, \$args) $body);
+#   };
+# }
+# keyword methodn (Ident $name, Block $body) {
+#   return qq{
+#     \$Type_Info->add_method_opv($name => sub (\$self, \$) $body);
+#   };
+# }
+
 sub import {
   my $targ = caller;
   my ($name) = $targ =~ m/^NXCL::(\w+)T$/
@@ -21,40 +60,40 @@ sub import {
     *{"${targ}::_make"} = $info->curry::make;
     *{"${targ}::Type_Info"} = \$info;
   }
-  keyword export (Ident $name, List $args, Block $body) {
-    return qq{
-      sub $name $args $body
-      \$Type_Info->add_export("$name", \\\&${name});
+  my $S = ' ';
+  Keyword::Simple::define export => sub {
+    ${$_[0]} =~ s{
+      \s* (\w+)
+    }{
+      \$Type_Info->add_export("$1", \\\&$1);
+      sub $1$S
+    }x;
+  };
+  foreach my $kwset ([ static => 'Type_' ], [ method => 'Inst_' ]) {
+    my ($base, $prefix) = @$kwset;
+    Keyword::Simple::define $base => sub {
+      ${$_[0]} =~ s{
+        \s* (\w+)
+      }{
+        \$Type_Info->add_${base}_apv("$1", \\\&${prefix}$1);
+        sub ${prefix}$1 (\$self, \$args)$S
+      }x;
     };
-  }
-  keyword static (Ident $name, Block $body) {
-    return qq{
-      \$Type_Info->add_static_apv($name => sub (\$self, \$args) $body);
+    Keyword::Simple::define "${base}x" => sub {
+      ${$_[0]} =~ s{
+        \s* (\w+)
+      }{
+        \$Type_Info->add_${base}_opv("$1", \\\&${prefix}$1);
+        sub ${prefix}$1 (\$self, \$args)$S
+      }x;
     };
-  }
-  keyword staticx (Ident $name, Block $body) {
-    return qq{
-      \$Type_Info->add_static_opv($name => sub (\$self, \$args) $body);
-    };
-  }
-  keyword staticn (Ident $name, Block $body) {
-    return qq{
-      \$Type_Info->add_static_opv($name => sub (\$self, \$) $body);
-    };
-  }
-  keyword method (Ident $name, Block $body) {
-    return qq{
-      \$Type_Info->add_method_apv($name => sub (\$self, \$args) $body);
-    };
-  }
-  keyword methodx (Ident $name, Block $body) {
-    return qq{
-      \$Type_Info->add_method_opv($name => sub (\$self, \$args) $body);
-    };
-  }
-  keyword methodn (Ident $name, Block $body) {
-    return qq{
-      \$Type_Info->add_method_opv($name => sub (\$self, \$) $body);
+    Keyword::Simple::define "${base}n" => sub {
+      ${$_[0]} =~ s{
+        \s* (\w+)
+      }{
+        \$Type_Info->add_${base}_opv("$1", \\\&${prefix}$1);
+        sub ${prefix}$1 (\$self, \$)$S
+      }x;
     };
   }
 }
