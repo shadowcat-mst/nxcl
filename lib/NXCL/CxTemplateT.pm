94e355651d315090d247ec607786c2a68fa6b9be
Matt S Trout <mst@shadowcat.co.uk>
2021-12-23 02:20:47 +0000

    convert CxTemplateT to TypeSyntax

--- lib/NXCL/CxTemplateT.pm
+++ lib/NXCL/CxTemplateT.pm
@@ -3,60 +3,58 @@ package NXCL::CxTemplateT;
 use Scalar::Util qw(weaken);
 use NXCL::ReprTypes qw(DictR);
 use NXCL::TypeFunctions qw(
-  make_Bool make_List cons_List make_Dict list_Combine
+  make_Bool make_List cons_List make_Dict cons_Combine list_Combine
   CxTemplate
 );
 use NXCL::Utils qw(uncons flatten raw panic);
-use NXCL::TypePackage;
+use NXCL::TypeSyntax;
 
-sub make ($scope, $dynamics) {
+export make ($scope, $dynamics) {
   _make DictR, => {
     scope => $scope,
     dynamics => $dynamics,
   };
-};
+}
 
-export make => \&make;
-
-static new => sub ($self, $args) {
+staticx new {
   my ($scope, $dynamics) = flatten($args);
   JUST make $scope, $dynamics;
 };
 
-method is_active => sub ($self, $) {
+methodn is_active {
   JUST make_Bool 0;
-};
+}
 
-method return => sub ($self, $args) {
+methodx return {
   panic "Can't return from a CxTemplate";
-};
+}
 
-method defer => sub ($self, $args) {
+methodx defer {
   panic "Can't register defer callbacks on a CxTemplate";
 };
 
-wrap method get_dynamic_value => sub ($self, $args) {
+method get_dynamic_value {
   my $name = raw((uncons($args))[0]);
   my $dyn = raw(raw($self)->{dynamics});
   panic "No dynamic value for ${name}"
     unless my $value = $dyn->{$name};
   return JUST $value;
-};
+}
 
-wrap method set_dynamic_value => sub ($self, $args) {
+method set_dynamic_value {
   my ($namep, $value) = flatten($args);
   my $name = raw($namep);
   my $dyn = raw(raw($self)->{dynamics});
   my $new_dyn = { %{$dyn}, $name => $value };
   raw($self)->{dynamics} = make_Dict($new_dyn);
   return JUST $value;
-};
+}
 
-method scope => sub ($self, $args) {
+methodn scope {
   return JUST raw($self)->{scope};
 };
 
-wrap method eval => my $eval = sub ($self, $args) {
+method eval {
   my ($expr) = uncons($args);
   my ($scope, $dyn_dict) = @{raw($self)}{qw(scope dynamics)};
   return (
@@ -64,13 +62,13 @@ wrap method eval => my $eval = sub ($self, $args) {
     EVAL($expr),
     LCTX(undef),
   );
-};
+}
 
-wrap method call => sub ($self, $args) {
-  $eval->($self, make_List(list_Combine($args)));
-};
+method call {
+  Inst_eval($self, make_List(list_Combine($args)));
+}
 
-wrap method derive => sub ($self, $args) {
+method derive {
   my ($scope, $dyn_dict) = @{raw($self)}{qw(scope dynamics)};
   return (
     CALL(derive => cons_List($scope, $args)),
@@ -78,6 +76,6 @@ wrap method derive => sub ($self, $args) {
     CONS(CxTemplate),
     CALL('new')
   );
-};
+}
 
 1;
