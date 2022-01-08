7d0a91555e2792f813713fa96036b63745af48cb
Matt S Trout <mst@shadowcat.co.uk>
2022-01-08 00:46:00 +0000

    xcl formatted trace output

---
+++ lib/NXCL/XTrace.pm
@@ -0,0 +1,24 @@
+package NXCL::XTrace;
+
+use NXCL::Package;
+use NXCL::Environment;
+use NXCL::ValueBuilders;
+
+sub import {
+  $NXCL::Environment::DEFAULT_TRACE_CB = \&DEBUG_WARN;
+}
+
+our $Trace_Env = NXCL::Environment->new(trace_cb => undef);
+
+sub DEBUG_WARN ($cxs, $ops) {
+  my $next_op = $ops->[-1];
+  warn join(' ',
+    map +(
+      ref()
+        ? $Trace_Env->eval(Cmb Call to_xcl_string => $_)->xcl_raw_value
+        : $_//'NULL'
+    ), @$next_op
+  )."\n";
+}
+
+1;
