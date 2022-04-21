20e85239f9a658f4f5537836aebf2c1109fb4d39
Matt S Trout <mst@shadowcat.co.uk>
2022-04-21 00:17:45 +0000

    add redefine warnings to the fatal list

--- lib/NXCL/Package.pm
+++ lib/NXCL/Package.pm
@@ -9,7 +9,7 @@ sub import {
   warnings->import::into(1);
   feature->import::into(1, ':5.16');
   experimental->import::into(1, 'signatures');
-  warnings->import::into(1, FATAL => 'uninitialized');
+  warnings->import::into(1, FATAL => qw(uninitialized redefine));
   warnings->unimport::out_of(1, 'once');
   NXCL::Require->import::into(1);
 }
