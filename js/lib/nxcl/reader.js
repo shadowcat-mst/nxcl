2af3c7302311b1e36608574419c41c9d4140e018
Matt S Trout <mst@shadowcat.co.uk>
2023-10-24 17:23:28 +0000

    expand out some of the reader code

--- js/lib/nxcl/reader.js
+++ js/lib/nxcl/reader.js
@@ -17,10 +17,14 @@ const TOK_TYPES = {
   word: WORD_START,
   number: DIGITS,
   qstring: "'",
-  comma: ",",
-  call: "[",
+  comma: ',',
+  semicolon: ';',
+  call: '[',
+  call_end: ']',
   list: '(',
+  list_end: ')',
   block: '{',
+  block_end: '}',
 };
 
 const TOK_START = Object.fromEntries(
@@ -32,6 +36,12 @@ const TOK_START = Object.fromEntries(
 const TOK_TYPES_CONTD = {
   ...TOK_TYPES,
   word: WORD_CHARS,
+  call: "\\[",
+  call_end: "\\]",
+  list: "\\(",
+  list_end: "\\)",
+  block: "\\{",
+  block_end: "\\}",
 };
 
 const TOK_MATCH = {
@@ -41,22 +51,40 @@ const TOK_MATCH = {
   qstring: /'(.*?(?<=[^\\])(?:\\\\)*)'/s,
 };
 
+const ATOM_TYPES = [
+  'number', 'word', 'symbol', 'qstring', 'call', 'list', 'block'
+];
+
+const IS_DELIMITED = Object.from_entries(
+  [ 'call', 'list', 'block' ].map(v => [ v, true ])
+)
+
+const SEQ_SEP = {
+  list: 'comma',
+  call: 'semicolon',
+  block: 'semicolon',
+};
+
 class ReadState {
 
   pos = 0;
   line = 1;
   linepos = 0;
 
-  constructor (string, source) {
-    this.string = string;
+  constructor (source) {
+    this.string = source.string;
     this.source = source;
   }
 
-  peekType () {
-    return TOK_START[this.string.charAt(0)];
+  peekChar () {
+    return this.string.charAt(0);
   }
 
-  extractNext () {
+  peekType () {
+    return TOK_START[this.peekChar()];
+  }
+
+  extractToken () {
     let { pos. line, linepos, string, source } = this;
     let start = { pos. line, linepos };
     let type = this.peekType();
@@ -74,5 +102,73 @@ class ReadState {
     let end = { pos, line, linepos };
     Object.assign(this, { ...end, string });
     return { type, value, start, end };
+  }
+
+  extractAtom () {
+    if (IS_DELIMITED[this.peekType()]) {
+      return this.extractDelimited();
+    }
+    return this.extractToken();
+  }
+
+  extractMaybeCompound () {
+    let found = this.extractSequenceOf(ATOM_TYPES);
+    if (!found.length) {
+      throw "WHAT";
+    }
+    if (found.length == 1) {
+      return found[0];
+    }
+    return {
+      type: 'compound',
+      value: found,
+      start: found.at(0).start,
+      end: found.at(-1).end,
+    };
+  }
+
+  extractSequenceOf (allowedTypes) {
+    let isAllowed = Object.from_entries(allowedTypes.map(t => [t, t]));
+    let found = [];
+    let nextType;
+    while (nextType = this.peekType()) {
+      if (!nextType in allowedTypes) {
+        break;
+      }
+      found.push(this.extractAtom());
+    }
+    return found;
+  }
+
+  extractDelimited () {
+    let type = this.peekType();
+    let delimiter_start = this.extractToken();
+    let { start } = delimiter_start;
+    let allowedTypes = [
+      ...ATOM_TYPES, 'ws', SEQ_SEP[type],
+    ];
+    let found = this.extractSequenceOf(allowedTypes);
+    if (this.peekType() != type + '_end') {
+      throw "FAIL";
+    }
+    let delimiter_end = this.extractToken();
+    let { end } = delimiter_end;
+    let reducer = (type == 'list' ? 'reduceExprList' : 'reduceMaybeExprSeq');
+    let value = this[reducer](found);
+    return {
+      type,
+      delimiter_start,
+      delimiter_end,
+      value,
+      start,
+      end,
+    };
+  }
+}
+
+export default class Reader {
+
+  parse (source) {
+    let st = new ReadState(source);
   }
 }
