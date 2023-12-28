83f1451906c748785fcf7adff0f74d19f9b994a8
Matt S Trout <mst@shadowcat.co.uk>
2023-12-28 22:16:59 +0000

    switch constants over to lazyObject pattern

--- js/lib/nxcl/constants.js
+++ js/lib/nxcl/constants.js
@@ -1,18 +1,20 @@
 "use strict";
 
-const qw = s => s.split(' ');
+function lazyObject(builder) {
+  let obj, proxy = new Proxy({}, {
+    get (target, prop, receiver) {
+      return obj[prop] = builder(prop);
+    }
+  });
+  return obj = Object.create(proxy);
+}
 
-const make = (src) => Object.freeze(
-  Object.assign(
-    Object.create(null),
-    (src instanceof Array ? Object.from_entries(src) : src),
-  )
-);
+function cascade(prefix, Maker) {
+  return lazyObject(prop => Maker(`${prefix}.${prop}`));
+}
 
-const symSet = (name, values) => [
-  name, make(values.map(v => [ v, Symbol([ name, v ].join('.')) ]),
-];
+const SymbolSet = (prefix) => cascade(prefix, Symbol);
 
-export const proto = make([
-  symSet('core', qw`EVAL CALL ASSIGN_VALUE ASSIGN_VIA_CALL`),
-]);
+const SymbolSetTree = (prefix) => cascade(prefix, SymbolSet);
+
+export const proto = SymbolSetTree('xcl.protocol');
