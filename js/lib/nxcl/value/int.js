53c27c1acd76ca1e38d61fd7469a3245391d70cd
Matt S Trout <mst@shadowcat.co.uk>
2024-03-15 18:23:24 +0000

    switch to simple 2-arg plus for the moment

--- js/lib/nxcl/value/int.js
+++ js/lib/nxcl/value/int.js
@@ -3,9 +3,11 @@ import { Value } from "../value.js";
 
 export class Int extends Value {
 
-  *[proto.numeric.plus] (cx, args) {
-    let sum = args.reduce((a, b) => a + b.data, this.data);
-    return new this.constructor({ ...this, data: sum });
+  *[proto.numeric.plus] (cx, [ otherp ]) {
+    let other = yield* cx.eval(otherp);
+    if (!other instanceof Int) throw "ARGH";
+    let value = this.value + other.value;
+    return new this.constructor({ ...this, value });
   }
 
   *[proto.numeric.to_int] () { return this }
