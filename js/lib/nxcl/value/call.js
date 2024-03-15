43bdb585faf9720dd795efc4beac7109fea005a0
Matt S Trout <mst@shadowcat.co.uk>
2024-03-15 20:26:15 +0000

    rename data to contents, remove rewriteOps() for now

--- js/lib/nxcl/value/call.js
+++ js/lib/nxcl/value/call.js
@@ -1,18 +1,17 @@
 import { proto } from "../constants.js";
 import { Value } from "../value.js";
-import { rewriteOps } from "../valuehelpers.js";
 
 export class Call extends Value {
 
   *[proto.core.CALL] (cx, args) {
-    let call = new this.constructor({
-      ...this, data: [ ...this.data, ...args ],
+    let call = !args.length ? this : new this.constructor({
+      ...this, contents: [ ...this.contents, ...args ],
     });
     return yield* cx.eval(call);
   }
 
   *[proto.core.EVAL] (cx) {
-    let [ combinerp, ...args ] = rewriteOps(cx, this).data;
+    let [ combinerp, ...args ] = this.contents;
     let combiner = yield* cx.eval(combinerp);
     return yield* cx.call(combiner, args);
   }
