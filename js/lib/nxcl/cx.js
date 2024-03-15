c6185c22261044e6edc599d9ce1603e41e0f19b1
Matt S Trout <mst@shadowcat.co.uk>
2024-03-15 18:22:47 +0000

    missing proto import

--- js/lib/nxcl/cx.js
+++ js/lib/nxcl/cx.js
@@ -1,3 +1,5 @@
+import { proto } from "./constants.js";
+
 export class Cx {
 
   constructor (args) {
@@ -7,9 +9,11 @@ export class Cx {
   eval (val) {
     return val[proto.core.EVAL](this);
   }
+
   call (val, args) {
     return val[proto.core.CALL](this, args);
   }
+
   send (val, messageId, args) {
     return val[messageId](this, args);
   }
