606269d25d93d98b417a9da36b776d09101dd3fb
Matt S Trout <mst@shadowcat.co.uk>
2024-03-16 20:58:25 +0000

    eval(Call) in callint sketch

--- js/sketches/callint.js
+++ js/sketches/callint.js
@@ -1,5 +1,5 @@
 import { proto } from "../lib/nxcl/constants.js";
-import { Message, Int } from "../lib/nxcl/valuetypes.js";
+import { Message, Int, Call } from "../lib/nxcl/valuetypes.js";
 import { Cx } from "../lib/nxcl/cx.js";
 
 let two = new Int({ value: 2 });
@@ -12,7 +12,11 @@ let message = new Message({
   args: []
 });
 
-let result = cx.call(message, [ two, three ]);
+let call = new Call({
+  contents: [ message, two, three ],
+});
+
+let result = cx.eval(call);
 
 let next;
 while (!(next = result.next()).done) {
