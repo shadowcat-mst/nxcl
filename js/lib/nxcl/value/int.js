2a9d490a28c9e9c07413b67ca1a86d6b3c69f772
Matt S Trout <mst@shadowcat.co.uk>
2024-03-23 23:17:27 +0000

    coerce to int if possible for plus

--- js/lib/nxcl/value/int.js
+++ js/lib/nxcl/value/int.js
@@ -5,7 +5,9 @@ export class Int extends Value {
 
   *[proto.numeric.plus] (cx, [ otherp ]) {
     let other = yield* cx.eval(otherp);
-    if (!other instanceof Int) throw "ARGH";
+    if (!(other instanceof Int)) {
+      other = yield* cx.send(other, proto.numeric.to_int);
+    }
     let value = this.value + other.value;
     return new this.constructor({ ...this, value });
   }
