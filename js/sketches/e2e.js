87a4814f947ac2bc659e89b47dcf86d120ce65ae
Matt S Trout <mst@shadowcat.co.uk>
2024-03-23 22:46:28 +0000

    first sketch of e2e test

---
+++ js/sketches/e2e.js
@@ -0,0 +1,31 @@
+import { proto } from "../lib/nxcl/constants.js";
+import { Message, Int, Call, Name, Val } from "../lib/nxcl/valuetypes.js";
+import { Cx } from "../lib/nxcl/cx.js";
+import { Scope } from "../lib/nxcl/scope.js";
+import { Reader } from "../lib/nxcl/reader.js";
+
+let reader = new Reader();
+
+let call = reader.read({ string: "+ 2 x" });
+
+let three = new Int({ value: 3 });
+
+let plus = new Message({
+  message: proto.numeric.plus,
+  args: []
+});
+
+let scopeData = {
+  '+': new Val({ value: plus }),
+  'x': new Val({ value: three }),
+};
+
+let cx = new Cx({ scope: new Scope({ proto: scopeData }) });
+
+let result = cx.eval(call);
+
+let next;
+while (!(next = result.next()).done) {
+  console.log('Yield:', next.value);
+}
+console.log('Value:', next.value.value);
