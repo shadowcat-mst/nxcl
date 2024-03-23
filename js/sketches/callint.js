cb859cfa0a95bef054c3e11360c05e431731f2df
Matt S Trout <mst@shadowcat.co.uk>
2024-03-23 22:46:15 +0000

    symbol using monkey callint

--- js/sketches/callint.js
+++ js/sketches/callint.js
@@ -1,25 +1,31 @@
 import { proto } from "../lib/nxcl/constants.js";
-import { Message, Int, Call } from "../lib/nxcl/valuetypes.js";
+import { Message, Int, Call, Name, Val } from "../lib/nxcl/valuetypes.js";
 import { Cx } from "../lib/nxcl/cx.js";
+import { Scope } from "../lib/nxcl/scope.js";
 
 let two = new Int({ value: 2 });
 let three = new Int({ value: 3 });
 
-let cx = new Cx({});
-
-let message = new Message({
+let plus = new Message({
   message: proto.numeric.plus,
   args: []
 });
 
+let scopeData = {
+  '+': new Val({ value: plus }),
+  'three': new Val({ value: three }),
+};
+
+let cx = new Cx({ scope: new Scope({ proto: scopeData }) });
+
 let call = new Call({
-  contents: [ message, two, three ],
+  contents: [ new Name({ value: '+' }), two, new Name({ value: 'three' }) ],
 });
 
 let result = cx.eval(call);
 
 let next;
 while (!(next = result.next()).done) {
-  console.log('Yield: ', next.value);
+  console.log('Yield:', next.value);
 }
-console.log('Value: ', next.value);
+console.log('Value:', next.value.value);
