2638aa91b78ef5483d28f6e1f296cf325e431bf8
Matt S Trout <mst@shadowcat.co.uk>
2024-03-31 21:25:17 +0000

    very basic debug printing

--- js/lib/nxcl/value.js
+++ js/lib/nxcl/value.js
@@ -6,4 +6,16 @@ export class Value {
   }
 
   *[proto.core.EVAL] () { return this }
+
+  toExternalString () {
+    return this.constructor.name + '(' + this.valueToExternalString() + ')';
+  }
+
+  valueToExternalString () {
+    if (Object.hasOwn(this, 'value')) return this.value.toString();
+    if (Object.hasOwn(this, 'contents')) {
+      return this.contents.map(v => v.toExternalString()).join(', ');
+    }
+    throw "Neither .value nor .contents present on Value object; WTF?!";
+  }
 }
