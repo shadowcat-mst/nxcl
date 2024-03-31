d2dc38a7281c7c03b8b67a29983ddce02bc3e8fa
Matt S Trout <mst@shadowcat.co.uk>
2024-03-31 21:22:39 +0000

    valuehelpers now at least a runnable sketch

--- js/lib/nxcl/valuehelpers.js
+++ js/lib/nxcl/valuehelpers.js
@@ -1,36 +1,59 @@
 import { Name } from "./value/name.js";
 import { Call } from "./value/call.js";
 
-export function *rewriteOps (orig, isOp, recurse) {
-  let { value, constructor } = orig;
-  if (value.length < 3) return orig;
-  let bestOp;
-  let ends = { 0: true, [value.length - 1]: true };
-  for (let idx in value) {
-    if (idx in ends) continue;
-    let el = value[idx];
-    if (el instanceof Name) {
-      let opInfo;
-      if (opInfo = isOp(el)) {
-        if (!bestOp || opInfo.precedence < bestOp.opInfo.precedence) {
-          bestOp = { idx, opInfo };
-        }
-      }
-    }
+function findBest (haystack, isOp) {
+  let ends = { 0: true, [haystack.length - 1]: true };
+  let best = [ ...haystack.keys() ]
+    .filter(idx => (!ends[idx]) && (haystack[idx] instanceof Name))
+    .flatMap((idx, op) => (
+      (op = isOp(haystack[idx]))
+        ? [{ ...op, idx }]
+      : []
+    ))
+    .reduce(
+       (a, v) => (v.precedence < a.precedence ? v : a),
+       { precedence: Number.MAX_SAFE_INTEGER },
+    );
+  if (best.idx) return best;
+  return null;
+}
+
+export function rewriteOps (orig, isOp) {
+
+  if (!Object.hasOwn(orig, 'contents')) return orig;
+
+  let recurse = v => rewriteOps(v, isOp);
+
+  let { contents, constructor } = orig;
+
+  if (contents.length < 3) {
+    return new constructor({
+      contents: contents.map(recurse)
+    });
   }
+
+  let bestOp = findBest(contents, isOp);
   if (!bestOp) return orig;
-  let { idx, opInfo } = bestOp;
+
+  let { idx, tightLeft, tightRight } = bestOp;
+
   let [ before, op, after ] = [
-    value.slice(0, idx),
-    value.at(idx),
-    value.slice(idx+1),
+    contents.slice(0, idx),
+    contents.at(idx),
+    contents.slice(idx+1),
   ];
-  let lhsp = opInfo.tightLeft ? [ before.pop() ] : before.splice(0);
-  let rhsp = opInfo.tightRight ? [ after.shift() ] : after.splice(0);
+
+  let lhsp = tightLeft ? [ before.pop() ] : before.splice(0);
+  let rhsp = tightRight ? [ after.shift() ] : after.splice(0);
+
   let [ lhs, rhs ] = [ lhsp, rhsp ].map(
     p => p.length == 1 ? p[0] : new constructor(p)
   );
-  let opCall = new Call([ op, recurse(lhs), recurse(rhs) ]);
-  if (!before && !after) return opCall;
-  return new constructor([ ...recurse(before), opCall, ...recurse(after) ]);
+
+  let opCall = new Call({ contents: [ op, recurse(lhs), recurse(rhs) ] });
+
+  if (!before.length && !after.length) return opCall;
+  return new constructor({
+    contents: [ ...before.map(recurse), opCall, ...after.map(recurse) ]
+  });
 }
