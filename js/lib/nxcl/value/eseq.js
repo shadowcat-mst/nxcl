e0ce36279ff731e4831bbcf398e7240841b46f32
Matt S Trout <mst@shadowcat.co.uk>
2024-04-01 20:00:22 +0000

    eseq CALL implementation

--- js/lib/nxcl/value/eseq.js
+++ js/lib/nxcl/value/eseq.js
@@ -2,5 +2,10 @@ import { proto } from "../constants.js";
 import { Value } from "../value.js";
 
 export class ESeq extends Value {
-  constructor () { throw "NYI" }
+  *[proto.core.CALL] (cx, args) {
+    let contents = [ ...this.contents ];
+    let last = contents.pop();
+    for (let expr of contents) { yield* cx.eval(expr) }
+    return yield* cx.eval(last);
+  }
 }
