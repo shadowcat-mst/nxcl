88752c8a18b429861382c133da48fa6ed269cd26
Matt S Trout <mst@shadowcat.co.uk>
2024-04-08 22:06:42 +0000

    move op rewriting out of Compound and refactor

--- js/lib/nxcl/value/compound.js
+++ js/lib/nxcl/value/compound.js
@@ -1,19 +1,14 @@
 import { proto } from "../constants.js";
-import { rewriteOps } from "../valuehelpers.js";
 import { Value } from "../value.js";
 import { List } from "./list.js";
 
 export class Compound extends Value {
 
   *[proto.core.EVAL] (cx) {
-    let rewritten = rewriteOps(cx, this);
-    if (!rewritten instanceof Compound) {
-      return yield* cx.eval(rewritten);
-    }
-    let [ first, ...rest ] = rewritten.data;
+    let [ first, ...rest ] = this.contents;
     let cur = yield* cx.eval(first);
     for (let r of rest) {
-      cur = yield* cx.call(cur, r instanceof List ? r : new List(r));
+      cur = yield* cx.call(cur, r instanceof List ? r.contents : [r]);
     }
     return cur;
   }
