0229650a1594476f6bfc5a0e087791fb7567f326
Matt S Trout <mst@shadowcat.co.uk>
2024-04-08 22:07:09 +0000

    temporary? EVAL+CALL support for ESeq

--- js/lib/nxcl/value/eseq.js
+++ js/lib/nxcl/value/eseq.js
@@ -2,7 +2,13 @@ import { proto } from "../constants.js";
 import { Value } from "../value.js";
 
 export class ESeq extends Value {
+
   *[proto.core.CALL] (cx, args) {
+    // not quite right but
+    return this[proto.core.EVAL](cx, args);
+  }
+
+  *[proto.core.EVAL] (cx, args) {
     let contents = [ ...this.contents ];
     let last = contents.pop();
     for (let expr of contents) { yield* cx.eval(expr) }
