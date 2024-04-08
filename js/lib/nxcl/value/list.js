e9fda5bb690b5a2fc31069e5d98bb5e58b7e4387
Matt S Trout <mst@shadowcat.co.uk>
2024-04-08 22:10:28 +0000

    remove NYI from List

--- js/lib/nxcl/value/list.js
+++ js/lib/nxcl/value/list.js
@@ -2,5 +2,5 @@ import { proto } from "../constants.js";
 import { Value } from "../value.js";
 
 export class List extends Value {
-  constructor () { throw "NYI" }
+  // currently only a container
 }
