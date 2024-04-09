70607aa49427740aa31b2682163b102c1dd3ec8c
Matt S Trout <mst@shadowcat.co.uk>
2024-04-09 17:30:31 +0000

    more tweaks to lex.js sketch

--- js/sketches/lex.js
+++ js/sketches/lex.js
@@ -1,11 +1,6 @@
 #!/usr/bin/env -S bun run
 import { Reader } from "../lib/nxcl/reader.js";
 
-let reader = new Reader();
-
-// let type = 'parse';
-let type = 'read';
-
-console.log(reader[type]({
+console.log((new Reader())[Bun.argv[3] ?? 'read']({
   string: (Bun.argv[2] ?? "+ x 1"),
-}).toExternalString());
+}));
