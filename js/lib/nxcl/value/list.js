c1a4b2b33231a0c643ad96a16bb83c2fe8d0845a
Matt S Trout <mst@shadowcat.co.uk>
2024-04-14 22:54:01 +0000

    add lmap and at methods to List

--- js/lib/nxcl/value/list.js
+++ js/lib/nxcl/value/list.js
@@ -1,6 +1,32 @@
-import { proto } from "../constants.js";
+import { proto, pub } from "../constants.js";
 import { Value } from "../value.js";
+import { Int } from "./int.js";
 
 export class List extends Value {
-  // currently only a container
+
+  [pub.lmap] (...args) { return this[proto.collection.lmap](...args) }
+
+  *[proto.collection.lmap] (cx, [ cbp ]) {
+    let cb = yield* cx.eval(cbp);
+    let mapped = [];
+    for (let v of this.contents) {
+      let ret = yield* cx.call(cb, [ v ]);
+      // if (!(ret instanceof List)) throw "ARGH";
+      if (!(ret instanceof List)) {
+        ret = new List({ contents: [ret] });
+      }
+      mapped.push(...ret.contents);
+    }
+    return new this.constructor({ contents: mapped });
+  }
+
+  *[proto.collection.at] (cx, [ atp ]) {
+    let at = yield* cx.eval(atp);
+    if (!(at instanceof Int)) {
+      at = yield* Int.prototype._toInt(cx, at);
+    }
+    return this.contents[at.value];
+  }
+
+  [proto.core.CALL] (...args) { return this[proto.collection.at](...args) }
 }
