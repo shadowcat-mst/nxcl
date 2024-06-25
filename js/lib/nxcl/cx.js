d40656fab948ee19dc9559773a778d2ea95e86e7
Matt S Trout <mst@shadowcat.co.uk>
2024-06-25 23:21:27 +0000

    add derive, improve debug yield

--- js/lib/nxcl/cx.js
+++ js/lib/nxcl/cx.js
@@ -6,6 +6,9 @@ export class Cx {
     Object.assign(this, opts);
   }
 
+  // these are equivalent to e.g.
+  // eval* (val) { return yield* this.send(...) }
+
   eval (val) {
     return this.send(val, proto.core.EVAL, []);
   }
@@ -15,12 +18,19 @@ export class Cx {
   }
 
   *send (val, message, args) {
-    yield [ val, message, args ];
+    yield [ '+', val, message, args ];
     let method = yield* this.scope.getMethod(this, val, message);
     if (!method) {
       // message may be a Symbol
       throw `No such method ${message.toString()} on ${val}`;
     }
-    return yield* method.call(val, this, args);
+    let ret = yield* method.call(val, this, args);
+    yield [ '-', ret ];
+    return ret;
+  }
+
+  *derive () {
+    let scope = yield* this.scope.derive();
+    return new this.constructor({ scope });
   }
 }
