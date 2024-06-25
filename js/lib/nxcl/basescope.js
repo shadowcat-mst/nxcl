f7c2689ac386632bbebd54370a49379dbc1c53b2
Matt S Trout <mst@shadowcat.co.uk>
2024-06-25 20:42:13 +0000

    fixup = to be operative wrt its LHS, add let

--- js/lib/nxcl/basescope.js
+++ js/lib/nxcl/basescope.js
@@ -1,6 +1,7 @@
 import { Scope } from "./scope.js";
 import { proto, pub } from "./constants.js";
-import { Val, Message, Bool } from "./valuetypes.js";
+import { Value, Val, Message, Bool } from "./valuetypes.js";
+import { letKeyword } from "./kw/let.js";
 
 let cells = {}, ops = {};
 
@@ -20,8 +21,19 @@ binOp('-', proto.numeric.minus, 0);
 binOp('==', proto.numeric.eq, 0);
 binOp('.', proto.core.DOT, 0, { tightRight });
 
+val('=', {
+  __proto__: Value.prototype,
+  *[proto.core.CALL] (cx, [ left, right ]) {
+    return yield* cx.send(left, proto.core.ASSIGN_VALUE, [ right ]);
+  },
+  toExternalString () { return 'Native(=)' },
+});
+ops['='] = { precedence: 0 };
+
 val('true', new Bool({ value: true }));
 val('false', new Bool({ value: false }));
+
+val('let', letKeyword);
 
 export function baseScope () {
   return new Scope({ cells, ops });
