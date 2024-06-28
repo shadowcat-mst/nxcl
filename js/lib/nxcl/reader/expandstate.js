c2eb09a11bdb8953acd0d3a756ff981ebfd127d0
Matt S Trout <mst@shadowcat.co.uk>
2024-06-28 16:20:15 +0000

    make ExpandState().extractOne error a bit better

--- js/lib/nxcl/reader/expandstate.js
+++ js/lib/nxcl/reader/expandstate.js
@@ -31,14 +31,17 @@ export class ExpandState {
   extractOne () {
     let node = { ...this.nextNode() };
     let Type = typeMap[node.type];
+    if (!Type) throw `no type map entry for ${node.type}`;
     delete node.type;
     let next = { };
     if (node.contents) {
       next.contents = this.subStateFor(node).extractAll();
       delete node.contents;
-    } else {
+    } else if (node.value) {
       next.value = node.value;
       delete node.value;
+    } else {
+      throw "NOTREACHED";
     }
     return new Type({ ...next, readerMeta: node });
   }
