1465f1a72a5933ad044b98b58973f9ec8175fc1b
Matt S Trout <mst@shadowcat.co.uk>
2024-08-01 16:20:51 +0000

    add defer for blocks

--- js/lib/nxcl/value/block.js
+++ js/lib/nxcl/value/block.js
@@ -1,10 +1,24 @@
 import { proto } from "../constants.js";
-import { Value } from "../value.js";
+import { Value, Val, Call } from "../valuetypes.js";
 
 export class Block extends Value {
 
   *[proto.core.CALL] (cx, args) {
-    let ecx = yield* cx.derive();
-    return yield* ecx.call(this.contents[0], []);
+    let scope = yield* cx.scope.derive();
+    let deferQueue = [];
+    yield* scope.setCell(cx, 'defer', new Val({ value: {
+      __proto__: Value.prototype,
+      *[proto.core.CALL] (cx, args) {
+        deferQueue.unshift([ cx.scope, new Call({ contents: args }) ]);
+      },
+    }}));
+    let ecx = yield* cx.derive({ scope });
+    try {
+      return yield* ecx.call(this.contents[0], []);
+    } finally {
+      for (let [ scope, call ] of deferQueue) {
+        yield* (yield* cx.derive({ scope })).eval(call);
+      }
+    }
   }
 }
