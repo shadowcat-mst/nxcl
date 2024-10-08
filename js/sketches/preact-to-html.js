e08b9868d41dc8e44524c579d915c68cc15f0e1f
Matt S Trout <mst@shadowcat.co.uk>
2024-10-08 20:50:51 +0000

    dump componentFunction, clean up hook

--- js/sketches/preact-to-html.js
+++ js/sketches/preact-to-html.js
@@ -1,31 +1,40 @@
 import render from 'preact-render-to-string/jsx';
 import { h, options } from 'preact';
 
+let RenderView = ({ view }) => view.render();
+
 let expandChildren = (children) => children.map(c =>
-  (typeof c == 'object' && 'componentFunction' in c)
-    ? h(c.componentFunction)
-    : (c instanceof Array)
+  (c instanceof View)
+    ? h(c)
+    : Array.isArray(c)
       ? expandChildren(c)
       : c
 );
 
 let oldHook = options.vnode;
 
-function vnode (vnode) {
-  if (oldHook) oldHook(vnode);
+function newHook (vnode) {
+  if (vnode.type instanceof View) {
+    vnode.props.view = vnode.type;
+    vnode.type = RenderView;
+  }
   let { children } = vnode.props;
-  if (children) vnode.props.children = expandChildren(
-    Array.isArray(children) ? children : [ children ]
-  );
+  if (children) {
+    vnode.props.children = expandChildren(
+      Array.isArray(children) ? children : [ children ]
+    );
+  }
+  if (oldHook) oldHook(vnode);
 }
 
-options.vnode = vnode;
+options.vnode = newHook;
 
 class View {
   constructor (args) {
     Object.assign(this, args);
-    this.componentFunction = () => this.render();
   }
+
+  toString () { return `[object ${this.constructor.name}]` }
 }
 
 class TestView extends View {
@@ -37,6 +46,8 @@ class TestView extends View {
 
 class SubView extends View { render () { return h('span', {}, 'foo') } }
 
-let foo = (new TestView()).componentFunction();
+let foo = h(new TestView());
 
-console.log(render(foo, null, { jsx: false }));
+let shallow = false;
+
+console.log(render(foo, null, { jsx: false, shallow }));
