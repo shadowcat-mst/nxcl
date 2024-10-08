58b9da903e805a45e4fdb4d57db100fc1ffe8acd
Matt S Trout <mst@shadowcat.co.uk>
2024-10-08 20:23:36 +0000

    view/subview rendering via options.vnode hook

--- js/sketches/preact-to-html.js
+++ js/sketches/preact-to-html.js
@@ -1,6 +1,42 @@
 import render from 'preact-render-to-string/jsx';
-import { h } from 'preact';
+import { h, options } from 'preact';
 
-let foo = h('div', {}, h('span', {}, 'foo'));
+let expandChildren = (children) => children.map(c =>
+  (typeof c == 'object' && 'componentFunction' in c)
+    ? h(c.componentFunction)
+    : (c instanceof Array)
+      ? expandChildren(c)
+      : c
+);
+
+let oldHook = options.vnode;
+
+function vnode (vnode) {
+  if (oldHook) oldHook(vnode);
+  let { children } = vnode.props;
+  if (children) vnode.props.children = expandChildren(
+    Array.isArray(children) ? children : [ children ]
+  );
+}
+
+options.vnode = vnode;
+
+class View {
+  constructor (args) {
+    Object.assign(this, args);
+    this.componentFunction = () => this.render();
+  }
+}
+
+class TestView extends View {
+
+  subview = new SubView();
+
+  render () { return h('div', {}, this.subview) }
+}
+
+class SubView extends View { render () { return h('span', {}, 'foo') } }
+
+let foo = (new TestView()).componentFunction();
 
 console.log(render(foo, null, { jsx: false }));
