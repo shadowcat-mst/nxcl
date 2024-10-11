189720e482f3c8f4bcf69750de5f91c09a16d47a
Matt S Trout <mst@shadowcat.co.uk>
2024-10-11 21:14:04 +0000

    factor TraceBuilder out of e2e-html sketch

--- js/sketches/e2e-html.js
+++ js/sketches/e2e-html.js
@@ -1,33 +1,12 @@
 import { Message } from "../src/nxcl/valuetypes.js";
 import { Interp } from "../src/nxcl/interp.js";
 import { TraceNode } from "../src/web/views/tracenode.js";
+import { TraceBuilder } from "../src/nxcl/tracebuilder.js";
 import render from 'preact-render-to-string/jsx';
 
-class TraceNodeModel {
-
-  // has: message, value, parent, children
-
-  children = [];
-
-  constructor (args) {
-    Object.assign(this, args);
-  }
-
-  addChildFor (message) {
-    let child = new this.constructor({ message, parent: this });
-    this.children.push(child);
-    return child;
-  }
-
-  completeNode (value) {
-    this.value = value;
-    return this.parent;
-  }
-
-}
-
 if (import.meta.main) {
-  await run(Bun.argv[2]??'1 + 3');
+  let vnode = await run(Bun.argv[2]??'1 + 3');
+  console.log(render(vnode, {}, { jsx: false }));
 } else {
   globalThis.runXcl = run;
 }
@@ -36,26 +15,11 @@ async function run (string) {
 
   let interp = new Interp();
 
-  let traceRoot = new TraceNodeModel();
+  let traceBuilder = new TraceBuilder();
 
   let result = await interp.evalString(string, {
-    eventHandlers: { trace: makeTraceHandler(traceRoot) },
+    eventHandlers: { trace: traceBuilder.handler },
   });
 
-  let vnode = (new TraceNode({ model: traceRoot.children[0] })).render();
-
-  console.log(render(vnode, {}, { jsx: false }));
-}
-
-function makeTraceHandler (traceRoot) {
-
-  let currentNode = traceRoot;
-
-  return (type, payload) => {
-    if (type == 'enter') {
-      currentNode = currentNode.addChildFor(payload);
-    } else if (type == 'leave') {
-      currentNode = currentNode.completeNode(payload);
-    }
-  };
+  return (new TraceNode({ model: traceBuilder.rootNode })).render();
 }
---
+++ js/src/nxcl/tracebuilder.js
@@ -0,0 +1,42 @@
+class Node {
+
+  // has: message, value, parent, children
+
+  children = [];
+
+  constructor (args) {
+    Object.assign(this, args);
+  }
+
+  addChildFor (message) {
+    let child = new this.constructor({ message, parent: this });
+    this.children.push(child);
+    return child;
+  }
+
+  completeNode (value) {
+    this.value = value;
+    return this.parent;
+  }
+}
+
+export class TraceBuilder {
+
+  addChildFor (message) {
+    // deliberately set no parent since the root object should only
+    // .completeNode() at the end of the traced evaluation
+
+    return this.rootNode = new Node({ message });
+  }
+
+  get handler () {
+    let current = this;
+    return (type, payload) => {
+      if (type == 'enter') {
+        current = current.addChildFor(payload);
+      } else if (type == 'leave') {
+        current = current.completeNode(payload);
+      }
+    };
+  }
+}
