e4d07b2139bd919979cf7bcfe8a5d1e88daccd16
Matt S Trout <mst@shadowcat.co.uk>
2024-10-12 21:02:40 +0000

    bundle libs.js and update server.js to serve it

--- js/sketches/server.js
+++ js/sketches/server.js
@@ -1,15 +1,21 @@
-const jsFile = /^\/((?:src|sketches)\/.*\.js)$/;
+const jsFile = /(?<=^\/)(src|sketches)\/(.*\.js)$/;
 
 Bun.serve({
   port: 4172,
   fetch (req) {
     let path = new URL(req.url).pathname;
     if (path == '/') {
+      console.log('Serving: index');
       return new Response(Bun.file('sketches/server/index.html'));
     }
     let m = path.match(jsFile);
     if (m) {
-      let jsPath = m[1];
+      let jsPath = m[0];
+      if (m[1] == 'src') {
+        let bundlePath = `bundle/${m[2]}`;
+        if (Bun.file(bundlePath).size) jsPath = bundlePath;
+      }
+      console.log(`Serving: ${jsPath}`);
       return new Response(Bun.file(jsPath));
     }
     return new Response('Nope', { status: 404 });
