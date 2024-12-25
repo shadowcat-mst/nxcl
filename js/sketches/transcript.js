b01b95a8d4755cc963e0e14f0a9db356a62a0390
Matt S Trout <mst@shadowcat.co.uk>
2024-12-25 00:11:32 +0000

    rewrite transcript running

--- js/sketches/transcript.js
+++ js/sketches/transcript.js
@@ -1,22 +1,38 @@
-import { Reactive } from '../src/web/reactive.js';
-import { Interp } from '../src/nxcl/interp.js';
-import { TraceBuilder } from '../src/nxcl/tracebuilder.js';
-import { mobx } from '../src/web/libs.js';
+import { Reactive } from '../src/web/reactive.js'
+import { Interp } from '../src/nxcl/interp.js'
+import { TraceBuilder } from '../src/nxcl/tracebuilder.js'
+import { mobx } from '../src/web/libs.js'
+
+class TranscriptRunner {
+
+  interp = new Interp()
+
+  async run (string) {
+    const traceBuilder = new TraceBuilder()
+
+    const { evalOpts } = traceBuilder
+
+    await this.interp.evalString(string, evalOpts)
+
+    return traceBuilder.rootNode
+  }
+}
 
 export class Transcript extends Reactive(Object, {
   set evaluations (v) { return mobx.observable(v ?? []) },
-  *appendEntry ({ string }) {
-    let traceBuilder = new TraceBuilder();
-
-    let { evalOpts } = traceBuilder;
-
-    let result = yield this.interp.evalString(string, evalOpts);
-
-    this.evaluations.push({ code: string, trace: traceBuilder.rootNode });
-  }
+  *recalculate () {
+    const runner = new TranscriptRunner()
+    for (const ev of this.evaluations) {
+      const trace = yield runner.run(ev.code)
+      ev.trace = trace
+      yield
+    }
+  },
 }) {
 
-  interp = new Interp();
-
-  constructor (args) { super(); Object.assign(this, args) }
+  constructor (args) {
+    super()
+    Object.assign(this, args)
+    this.recalculate()
+  }
 }
