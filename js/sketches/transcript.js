18a2d8dc3a5c64dd501bc6e26496e110358e6f66
Matt S Trout <mst@shadowcat.co.uk>
2024-12-25 23:44:29 +0000

    auto-recalculate

--- js/sketches/transcript.js
+++ js/sketches/transcript.js
@@ -2,6 +2,7 @@ import { Reactive } from '../src/web/reactive.js'
 import { Interp } from '../src/nxcl/interp.js'
 import { TraceBuilder } from '../src/nxcl/tracebuilder.js'
 import { mobx } from '../src/web/libs.js'
+import { ensureHiddenProp } from '../src/util/objects.js'
 
 class TranscriptRunner {
 
@@ -21,12 +22,18 @@ class TranscriptRunner {
 export class Transcript extends Reactive(Object, {
   set evaluations (v) { return mobx.observable(v ?? []) },
   *recalculate () {
+    const $reaction = 'recalculate$reaction';
+    const reaction = ensureHiddenProp(
+      this, $reaction,
+      () => new mobx.Reaction($reaction, () => this.recalculate())
+    )
     const runner = new TranscriptRunner()
     for (const ev of this.evaluations) {
       const trace = yield runner.run(ev.code)
       ev.trace = trace
       yield
     }
+    reaction.track(() => this.evaluations.map(v => v.code))
   },
 }) {
 
