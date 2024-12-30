84ad00dc294a03a882a2af68f91e67b18019f8ff
Matt S Trout <mst@shadowcat.co.uk>
2024-12-30 22:33:21 +0000

    document why preactOptions is separate

--- js/src/web/libs.js
+++ js/src/web/libs.js
@@ -9,6 +9,9 @@ import { default as jsxRenderToString } from 'preact-render-to-string/jsx'
 
 const renderToString = v => jsxRenderToString(v, {}, { jsx: false })
 
+// only things plugging in to preact should be accessing this so it gets its
+// own separate export whereas the others are relatively common/normal usage
+
 export { options as preactOptions }
 
 export const preact = {
