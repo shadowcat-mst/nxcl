46e0783b34c4af5dd92dcb079619044dbf5a4051
Matt S Trout <mst@shadowcat.co.uk>
2025-01-01 23:33:42 +0000

    make server sketch's static handling a function

--- js/sketches/server.js
+++ js/sketches/server.js
@@ -1,29 +1,33 @@
-const staticFile = /(?<=^\/)(css|src|sketches)\/(.*)$/;
+const staticFile = /(?<=^\/)(css|src|sketches)\/(.*)$/
 
-Bun.serve({
+export function staticHandler (path) {
+  if (path == '/') {
+    console.log('Serving: index')
+    return new Response(Bun.file('sketches/server/index.html'))
+  }
+  if (path == '/favicon.ico') {
+    console.log('Serving: favicon')
+    return new Response(Bun.file('sketches/server/favicon.ico'))
+  }
+  const m = path.match(staticFile)
+  if (m) {
+    const staticPath = m[0]
+    if (m[1] == 'src') {
+      const bundlePath = `bundle/${m[2]}`
+      if (Bun.file(bundlePath).size) staticPath = bundlePath
+    } else if (m[1] == 'css') {
+      staticPath = `sketches/server/css/${m[2]}`
+    }
+    console.log(`Serving: ${staticPath}`)
+    return new Response(Bun.file(staticPath))
+  }
+  return
+}
+
+if (import.meta.main) Bun.serve({
   port: 4172,
   fetch (req) {
-    let path = new URL(req.url).pathname;
-    if (path == '/') {
-      console.log('Serving: index');
-      return new Response(Bun.file('sketches/server/index.html'));
-    }
-    if (path == '/favicon.ico') {
-      console.log('Serving: favicon');
-      return new Response(Bun.file('sketches/server/favicon.ico'));
-    }
-    let m = path.match(staticFile);
-    if (m) {
-      let staticPath = m[0];
-      if (m[1] == 'src') {
-        let bundlePath = `bundle/${m[2]}`;
-        if (Bun.file(bundlePath).size) staticPath = bundlePath;
-      } else if (m[1] == 'css') {
-        staticPath = `sketches/server/css/${m[2]}`;
-      }
-      console.log(`Serving: ${staticPath}`);
-      return new Response(Bun.file(staticPath));
-    }
-    return new Response('Nope', { status: 404 });
+    const path = new URL(req.url).pathname
+    return staticHandler(path) ?? new Response('Nope', { status: 404 })
   }
-});
+})
