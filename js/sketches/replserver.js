7ac92cdac1dce7d2f7893ac10253f54d0d1f43b8
Matt S Trout <mst@shadowcat.co.uk>
2025-01-03 19:42:41 +0000

    update to newer stupidrpc, add name validation

--- js/sketches/replserver.js
+++ js/sketches/replserver.js
@@ -1,26 +1,46 @@
 import { staticHandler } from './server.js'
 import { startNexus, websocketHandlers } from '../src/stupidrpc/bunserver.js'
-import { readFile, writeFile } from 'node:fs/promises'
+import { opendir, readFile, writeFile } from 'node:fs/promises'
 
 const storePath = 'var/repl'
 
+const allowedStoreName = /^[a-zA-Z0-9_]+$/
+
 const callHandlers = {
+  async list () {
+    const dir = await opendir(storePath), names = []
+    for await (const dirent of dir) {
+      const m = dirent.name.match(/^(.*?)\.json$/)
+      if (m && m[1].match(allowedStoreName)) names.push(m[1])
+    }
+    return names
+  },
+  // not sure if I should be setting { encoding: 'utf8' } on both here
+  // but hopefully for the moment 'neither' will at least work consistently
   async load (name) {
+    if (!name.match(allowedStoreName)) throw "Invalid store name"
     const fileName = `${storePath}/${name}.json`
-    return JSON.parse(await readFile(fileName, { encoding: 'utf8' }))
+    return JSON.parse(await readFile(fileName))
   },
   async save (name, data) {
+    if (!name.match(allowedStoreName)) throw "Invalid store name"
     const fileName = `${storePath}/${name}.json`
-    await writeFile(fileName, JSON.stringify(data, null, 2))
+    await writeFile(fileName, JSON.stringify(data, null, 2) + '\n')
     return true
   }
+}
+
+function startCall (call, ...args) {
+  return callHandlers[call](...args)
 }
 
 if (import.meta.main) Bun.serve({
   port: 4172,
   fetch (req, server) {
-    const path = new URL(req.url).pathname
-    if (path == '/ws') return startNexus(req, server, callHandlers)
+    let path = new URL(req.url).pathname
+    if (path == '/ws') {
+      return startNexus(req, server, { startCall, debug: console })
+    }
     return staticHandler(path) ?? new Response('Nope', { status: 404 })
   },
   websocket: websocketHandlers,
